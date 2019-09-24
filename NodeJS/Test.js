e40a4035704ae8848c0c1fbec7ff43492553fc2a
skhorozian201 <sk201@hw.ac.uk>
2019-09-25 00:18:40 +0400

    Added NodeJS folder

---
+++ NodeJS/Test.js
@@ -0,0 +1,10 @@
+var group_member_names = ["","Sarkis Khorozian","","",""]; 
+
+
+console.log ("This is a test.");
+
+for (var i = 0; i < group_member_names.length; i++) {
+	if (i == 0)
+		console.log ("The students working on this group are:");
+	console.log ((i+1) + ") " + group_member_names[i]);
+}
