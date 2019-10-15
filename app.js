996a2342413c12ba3582912e9d18c419d5507f70
skhorozian201 <sk201@hw.ac.uk>
2019-10-15 12:58:02 +0400

    Added comments for clarification

--- app.js
+++ app.js
@@ -7,63 +7,178 @@ app.get ('/', function (req, res) {
 });
 app.use ('/client', express.static(__dirname + '/client'));
 
-serv.listen (2000);
+serv.listen (2000); //listens to port :2000
 console.log ("Server started...");
 
-var SOCKET_LIST = {};
+var SOCKET_LIST = {}; //List of connections
+var PLAYER_LIST = {}; //List of players
 
 var io = require ('socket.io') (serv,{});
 
+//Player Class
 class Player {
 
     constructor (id, name,) {
-        this.id = id;
-        this.name = name;
+        this.id = id; //Player ID
+        this.name = name; //Player Name
 
-        this.maxHealth = 100;
-        this.currHealth = this.maxHealth;
+        this.x_position = 200; //Player position on the x-axis
+        this.y_position = 200; //Player position on the y-axis
+
+        this.maxHealth = 300; //Player maximum health
+        this.currHealth = this.maxHealth; //Player CURRENT health
+
+        this.moveSpeed = 5; //Player movement speed
+
+        this.moveUpInput = false; //Is the player pressing 'W' key
+        this.moveDownInput = false; //Is the player pressing 'S' key
+        this.moveRightInput = false; //Is the player pressing 'D' key
+        this.moveLeftInput = false; //Is the player pressing 'A' key
+
+        //List of Functions:
+
+        //Perferably don't touch or use these.
+
+        //This is used to call all the functions in the list once every frame. 
+        //Parameters (player)
+        this.perFrameEvent = {}; 
+
+        //This is used to call all the functions in the list everytime the player deals damage.
+        //Parameters (player (this), player (damage taker), number (damage))
+        this.onDealingDamageEvent = {}; 
+
+        //This is used to call all the functions in the list everytime the player takes damage.
+        //Parameters (player (this), player (damage dealer), number (damage))
+        this.onTakingDamageEvent = {};
+
+        //Status effects
+        this.isDead = false; //This is death...
+        this.isImmune = false; //This is when the player is immune to damage
+        this.isUntargetable = false; //This is when the cannot be interacted with
+
+        
+
+
+    }
+
+    //Called to deal damage to this player
+    //damage is the number
+    //dealer is the player doing the damage
+    TakeDamage (damage, dealer) {
+
+        if (!this.isImmune) //If the player is not immune to damage
+            this.currHealth -= damage; //then subtract current health by damage
+        else 
+            damage = 0;
+
+        if (this.currHealth <= 0) { //If the player's current health drops to 0
+            this.currHealth = 0; //Current health never drops below 0
+            this.Death ();      //and if the player drops below 0
+        }
+
+        return damage; //Return the damage incase it changes... somehow...
+    }
+
+    //Called to restore current health to this player
+    //heal is the number
+    //healer is the player doing the healing
+    TakeHeal (heal, healer) {
+        this.currHealth += heal; //add current health by heal
+
+        if (this.currHealth > this.maxHealth) //If the player's health reaches its max
+            this.currentHealth = this.maxHealth; //then cap it at the MaxHealth
+
+        return heal; //Return the heal value incase it changes... somehow...
+    }
+
+    //Called when this player wants to deal damage
+    //damage is the number
+    //victim is the unfortunate player taking the damage
+    DealDamage (damage, victim) {
+
+        victim.TakeDamage (damage, this);
+    }
+
+    //Called when this player wants to heal
+    //heal is the number
+    //receiver is the player receiving the heal
+    SendHeal (heal, receiver) {
+        receiver.TakeDamage (heal, this)
+    }
+
+
+    //Called when the player drops to 0 current health
+    Death () {
+        this.isImmune = true;
+        this.isDead = true;
+        this.isUntargetable = true;
     }
 
 }
 
-
+//Please read Socket.io documentation as even I dont understand this
 io.sockets.on ('connection', function (socket){
-    socket.id = Math.random ();
-    socket.x = 200;
-    socket.y = 200;
+    socket.id = Math.random (); //creates a random ID for the new connection
+    SOCKET_LIST [socket.id] = socket; //adds the new socket to the list
 
-    SOCKET_LIST [socket.id] = socket;
+    var player = new Player (socket.id,"Player " + socket.id); //constructs a new Player instance
+    PLAYER_LIST [socket.id] = player; //adds the new player to the lsit
 
     console.log ('socket connection');
     
-    socket.on ('disconnect',function(){
-        delete SOCKET_LIST [socket.id];
+    socket.on ('disconnect',function(){ //When a player disconnects from the game
+        delete SOCKET_LIST [socket.id]; //remove them from the player and the socket list
+        delete PLAYER_LIST[socket.id];
         console.log ('socket disconnet');
     });
 
-    socket.on ('sendPosition',function (data){
-        socket.x += data.x * 10,
-        socket.y += data.y * 10  
+    socket.on ('sendMoveDirs',function (data) { //This is receive the data of the players movement input from the client
+        PLAYER_LIST [socket.id].moveUpInput = data.moveDirections[0],
+        PLAYER_LIST [socket.id].moveDownInput = data.moveDirections[1],
+        PLAYER_LIST [socket.id].moveRightInput = data.moveDirections[2],
+        PLAYER_LIST [socket.id].moveLeftInput = data.moveDirections[3]
     });
 }); 
 
+//This is the server's update function
+//This does everything that's time based
+//It is called 24 times a second
 setInterval (function () {
-    var pack = [];
+    var movePack = []; //The list of all player's position as a packet.
 
-    for (var i in SOCKET_LIST) {
-        var socket = SOCKET_LIST [i];
+    //This is called for every instance of player
+    //Use it as gameplay update function for each player
+    for (var i in PLAYER_LIST) {
+        var player = PLAYER_LIST [i];
 
-        pack.push ({
-            x:socket.x,
-            y:socket.y
+        //This move the player based on the input
+        if (player.moveUpInput) {
+            player.x_position += player.moveSpeed;
+        } else if (player.moveDownInput) {
+            player.x_position -= player.moveSpeed;            
+        }
+
+        if (player.moveRightInput) {
+            player.y_position += player.moveSpeed;
+        } else if (player.moveLeftInput) {
+            player.y_position -= player.moveSpeed;            
+        }
+
+        //This adds the new position data to the list
+        movePack.push ({
+            x: player.x_position,
+            y: player.y_position
         });
 
         
         
     }
+
+    //This is called for every socket (connection)
+    //This sends data to the client
     for (var i in SOCKET_LIST) {
-        var socket = SOCKET_LIST [i];
-        socket.emit ('newPosition', pack);
+        var socket = SOCKET_LIST [i]; 
+        socket.emit ('newPosition', movePack); //Sending position data to all connections about every player's position
     }
 
     
